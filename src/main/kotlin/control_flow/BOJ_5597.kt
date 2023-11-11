799c65dc6bc286dafdaf28ccc367c563b46a37c3
Jwhyee <kawnsdud@naver.com>
2023-11-11 15:36:55 +0900

    init

---
+++ src/main/kotlin/control_flow/BOJ_5597.kt
@@ -0,0 +1,14 @@
+package control_flow
+
+fun main() {
+    val list = arrayListOf<Int>()
+
+    repeat(28) {
+        list.add(readln().toInt())
+    }
+
+    for (i in 1..30) {
+        if(i !in list) println(i)
+    }
+
+}
