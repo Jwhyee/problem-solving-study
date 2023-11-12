e10c5f560a242abc91b5b2c8c981e9df531ab018
Jwhyee <kawnsdud@naver.com>
2023-11-12 14:49:22 +0900

    add BOJ_25314

---
+++ src/main/kotlin/control/BOJ_25314.kt
@@ -0,0 +1,15 @@
+package control
+
+import java.lang.StringBuilder
+
+fun main() {
+    val N = readln().toInt()
+    val sb = StringBuilder()
+
+    repeat(N / 4) {
+        sb.append("long ")
+    }
+    sb.append("int")
+
+    println(sb)
+}
