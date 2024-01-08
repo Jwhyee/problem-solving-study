0ce788cfff7f8702e331bd4af50f17cc223ab372
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2024-01-08 21:32:14 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 496 ms, Memory: 42468 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.kt
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.kt
@@ -1,22 +1,27 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 import java.util.StringTokenizer
 
-fun main() = with(System.`in`.bufferedReader()) {
+fun main() {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val br = BufferedReader(InputStreamReader(System.`in`))
     val st = StringTokenizer(readLine())
     val n = st.nextToken().toInt()
     val m = st.nextToken().toInt()
 
-    val bw = System.out.bufferedWriter()
     val nameMap = hashMapOf<String, Int>()
     val list = mutableListOf<String>()
 
     for (i in 1 .. n) {
-        val name = readLine()
+        val name = br.readLine()
         nameMap[name] = i
         list += name
     }
 
     for (i in 0 until m) {
-        val q = readLine()
+        val q = br.readLine()
         if (q[0].isDigit()) {
             bw.append(list[q.toInt() - 1])
             bw.newLine()
@@ -26,5 +31,6 @@ fun main() = with(System.`in`.bufferedReader()) {
         }
     }
     bw.flush()
+    br.close()
     bw.close()
 }
