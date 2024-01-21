5a005142140011065ccc5fe738c0393c34ca19df
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2024-01-21 19:00:29 +0900

    [Gold III] Title: 아기돼지와 늑대, Time: 180 ms, Memory: 22360 KB -BaekjoonHub

---
+++ 백준/Gold/16441. 아기돼지와 늑대/아기돼지와 늑대.kt
@@ -0,0 +1,81 @@
+import java.util.*
+
+private lateinit var map: Array<CharArray>
+private lateinit var visited: Array<BooleanArray>
+private val queue: Queue<Pair<Int, Int>> = LinkedList()
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+
+    map = Array(n) { CharArray(m) }
+    visited = Array(n) { BooleanArray(m) }
+
+    for (y in 0 until n) {
+        val arr = br.readLine().toCharArray()
+        for (x in 0 until m) {
+            val cur = arr[x]
+            map[y][x] = cur
+            if(cur == 'W') queue += (y to x)
+        }
+    }
+
+    bfs()
+
+    for (y in 0 until n) {
+        for (x in 0 until m) {
+            val cur = map[y][x]
+            val c = if(cur == '.' && !visited[y][x]) 'P' else cur
+            bw.append(c)
+        }
+        bw.newLine()
+    }
+
+    br.close()
+    bw.flush()
+    bw.close()
+}
+
+private val dx = intArrayOf(1, 0, -1, 0)
+private val dy = intArrayOf(0, -1, 0, 1)
+
+private fun bfs() {
+    while (queue.isNotEmpty()) {
+        val cur = queue.poll()
+        val y = cur.first
+        val x = cur.second
+
+        visited[y][x] = true
+
+        for (dir in 0..3) {
+            var ny = y + dy[dir]
+            var nx = x + dx[dir]
+
+            if (isValid(ny, nx) && !visited[ny][nx]) {
+                val stat = map[ny][nx]
+                // 다음 좌표가 빙판길일 경우
+                if (stat == '+') { //
+                    // 산 혹은 평원을 만날 때까지 이동
+                    while (isValid(ny + dy[dir], nx + dx[dir])) {
+                        if(map[ny][nx] != '+' || map[ny + dy[dir]][nx + dx[dir]] == '#') break
+                        ny += dy[dir]
+                        nx += dx[dir]
+                    }
+                }
+
+                // 빙판길을 통해 이동한 좌표에 대해 방문 처리
+                if (stat != '#' && !visited[ny][nx]) {
+                    visited[ny][nx] = true
+                    queue += ny to nx
+                }
+            }
+
+        }
+    }
+}
+
+private fun isValid(y: Int, x: Int): Boolean {
+    return y in map.indices && x in map[0].indices
+}
