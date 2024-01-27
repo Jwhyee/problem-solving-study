130cd0da2cefe0cb68809684acd70666e8174327
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2024-01-28 01:36:23 +0900

    [Gold IV] Title: 민준이와 마산 그리고 건우, Time: 420 ms, Memory: 25376 KB -BaekjoonHub

--- 백준/Gold/18223. 민준이와 마산 그리고 건우/민준이와 마산 그리고 건우.kt
+++ 백준/Gold/18223. 민준이와 마산 그리고 건우/민준이와 마산 그리고 건우.kt
@@ -6,13 +6,9 @@ private lateinit var distance: IntArray
 private var v = 0
 private var e = 0
 private var p = 0
-/**
- * 문제 이름(난이도) : 민준이와 마산 그리고 건우(GOL4)
- * 시간 : 464 ms
- * 메모리 : 28288 KB
- * 링크 : https://www.acmicpc.net/problem/18233
- */
+
 fun main() = with(System.`in`.bufferedReader()) {
+    val bw = System.out.bufferedWriter()
     var st = StringTokenizer(readLine())
     // 정점의 개수
     v = st.nextToken().toInt()
@@ -48,14 +44,16 @@ fun main() = with(System.`in`.bufferedReader()) {
     val len3 = distance[v]
 
     // 마산까지의 최소 거리가 건우를 픽업하고, 마산까지 가는 거리의 합과 같을 경우
-    if(len1 == (len2 + len3)) println("SAVE HIM")
-    else println("GOOD BYE")
+    if(len1 == (len2 + len3)) bw.append("SAVE HIM")
+    else bw.append("GOOD BYE")
+    bw.flush()
+    bw.close()
     close()
 }
 
 private fun dijkstra(start: Int) {
     // 정점, 거리에 대한 PQ (거리가 짧은 순서로 정렬)
-    val pq: Queue<Pair<Int, Int>> = PriorityQueue {a, b -> a.second - b.second}
+    val pq: Queue<Pair<Int, Int>> = PriorityQueue { a, b -> a.second - b.second }
 
     // 거리 배열 최댓값으로 초기화
     distance = IntArray(v + 1) { 10_001 }
@@ -74,7 +72,7 @@ private fun dijkstra(start: Int) {
             val nextCost = next.second
             // 다음 정점까지의 누적 거리
             val d = distance[curNode] + nextCost
-            
+
             // 다음 정점까지 최소 거리가 (현재 정점까지의 최소 거리 + 다음 정점까지의 거리)보다 클 경우
             // 다음 정점 최소 거리 초기화 후 pq에 다음 정점과 그에 대한 최소 거리 추가
             if (distance[nextNode] > d) {
