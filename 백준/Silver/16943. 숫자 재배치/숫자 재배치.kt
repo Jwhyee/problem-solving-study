a64f2472ace4079c5a38f52fbd601cade85eed8a
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2024-02-04 14:51:54 +0900

    [Silver I] Title: 숫자 재배치, Time: 532 ms, Memory: 42620 KB -BaekjoonHub

--- 백준/Silver/16943. 숫자 재배치/숫자 재배치.kt
+++ 백준/Silver/16943. 숫자 재배치/숫자 재배치.kt
@@ -1,9 +1,9 @@
-package back_tracking
-
+import java.lang.StringBuilder
 import java.util.StringTokenizer
 import kotlin.math.max
 
 private lateinit var visited: BooleanArray
+private val sb = StringBuilder()
 private var aSize = 0
 private var aStr = ""
 private var a = 0
@@ -16,19 +16,19 @@ fun main() = with(System.`in`.bufferedReader()){
     a = aStr.toInt()
     b = st.nextToken().toInt()
     visited = BooleanArray(aSize)
-    backTracking(0, "")
+    backTracking(0)
     println(max)
 }
 
-private fun backTracking(depth: Int, str: String) {
+private fun backTracking(depth: Int) {
     if (depth == aSize) {
-        val result = str
-        if (result.startsWith("0")) {
+        val st = sb.toString()
+        if (st.startsWith("0")) {
             return
         }
-        val _result = result.toInt()
-        if (_result in 0 until b) {
-            max = max(_result, max)
+        val result = st.toInt()
+        if (result in 0 until b) {
+            max = max(result, max)
         }
         return
     }
@@ -36,7 +36,9 @@ private fun backTracking(depth: Int, str: String) {
     for (i in 0 until aSize) {
         if (!visited[i]) {
             visited[i] = true
-            backTracking(depth + 1, str + aStr[i])
+            sb.append(aStr[i])
+            backTracking(depth + 1)
+            sb.deleteCharAt(depth)
             visited[i] = false
         }
     }
