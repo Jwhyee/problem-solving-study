3e3032acf33cb4fe2fceb2de4b15ce89dc3b2aab
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2024-02-10 23:28:17 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 252 ms, Memory: 17532 KB -BaekjoonHub

---
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.kt
@@ -0,0 +1,55 @@
+import java.util.StringTokenizer
+import kotlin.math.abs
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val bw = System.out.bufferedWriter()
+    val n = readLine().toInt()
+
+    val st = StringTokenizer(readLine())
+
+    val arr = IntArray(n) {
+        st.nextToken().toInt()
+    }
+
+    arr.sort()
+
+    var answer = Int.MAX_VALUE
+
+    for (i in 0 until n - 1) {
+        for (j in i + 1 until n) {
+            val sum1 = arr[i] + arr[j]
+            var left = 0
+            var right = n - 1
+
+            while (left < right) {
+                if (left == i || left == j) {
+                    left++
+                    continue
+                }
+                if (right == i || right == j) {
+                    right--
+                    continue
+                }
+
+                val sum2 = arr[left] + arr[right]
+                val result = sum1 - sum2
+
+                if (result < 0) right--
+                else if (result > 0) left++
+                else {
+                    bw.write("0")
+                    bw.close()
+                    close()
+                    return@with
+                }
+                answer = answer.coerceAtMost(abs(result))
+            }
+
+        }
+    }
+
+    bw.write("$answer")
+    bw.close()
+    close()
+
+}
