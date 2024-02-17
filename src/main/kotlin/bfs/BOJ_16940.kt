dae65bd8f4c46ef0013a1699322b09f2672e7e2c
Jwhyee <kawnsdud@naver.com>
2024-02-18 00:35:20 +0900

    [BOJ][GOL3] - BFS 스페셜 저지

--- src/main/kotlin/bfs/BOJ_16940.kt
+++ src/main/kotlin/bfs/BOJ_16940.kt
@@ -1,56 +1,77 @@
 package bfs
 
-import java.lang.StringBuilder
 import java.util.*
+
+private class Node (var parent: Int, var children: Int)
 
 /**
  * 문제 이름(난이도) : BFS 스페셜 저지 (GOL3)
- * 시간 : 11 ms
- * 메모리 : 11 KB
+ * 시간 : 620 ms
+ * 메모리 : 73328 KB
  * 링크 : https://www.acmicpc.net/problem/16940
  */
 fun main() = with(System.`in`.bufferedReader()) {
-   val sb = StringBuilder()
-
    val n = readLine().toInt()
+   val graph = Array<MutableList<Int>>(n + 1) { mutableListOf() }
 
-   val graph = MutableList<MutableList<Int>>(n) { mutableListOf() }
-
-   repeat(n - 1) { idx ->
+   repeat(n-1) {
       val st = StringTokenizer(readLine())
-      val parent = st.nextToken().toInt() - 1
-      if (idx == 0 && parent != 0) {
-         println("0")
-         return@with
-      }
-      val child = st.nextToken().toInt() - 1
+      val n1 = st.nextToken().toInt()
+      val n2 = st.nextToken().toInt()
 
-      graph[parent].add(child)
-      graph[child].add(parent)
+      graph[n1].add(n2)
+      graph[n2].add(n1)
    }
 
-   val answer = readLine()
-   val visited = BooleanArray(n)
+   val order = mutableListOf<Int>()
 
-   fun bfs() {
-      val queue: Queue<Int> = LinkedList()
-      queue += 0
+   val st = StringTokenizer(readLine())
+   while (st.hasMoreTokens()) {
+      order += st.nextToken().toInt()
+   }
 
-      while (queue.isNotEmpty()) {
-         val parent = queue.poll()
-         sb.append("${parent + 1} ")
-         visited[parent] = true
+   val queue: Queue<Int> = LinkedList()
+   val nodes = Array(n + 1) { Node(0, 0) }
+   val visited = BooleanArray(n + 1)
 
-         for (child in graph[parent]) {
-            if(!visited[child]) {
-               queue += child
-            }
+   queue += 1
+   visited[1] = true
+
+   while (!queue.isEmpty()) {
+      val cur = queue.poll()
+
+      for (i in graph[cur]) {
+         if (!visited[i]) {
+            visited[i] = true
+            nodes[i].parent = cur
+            nodes[cur].children++
+            queue += i
          }
       }
    }
 
-   bfs()
-   if(sb.toString().trim() == answer) println("1")
-   else println("0")
+   val bw = System.out.bufferedWriter()
+
+   var parent = 1
+   var flag = true
+   for (i in 1 until n) {
+      val cur = order[i]
+      if (parent != nodes[cur].parent) {
+         flag = false
+         break
+      }
+
+      if (nodes[cur].children > 0) queue += cur
+      nodes[parent].children--
+      if (nodes[parent].children == 0 && i < n - 1) parent = queue.poll()
+   }
+
+   if(!flag) bw.append("0")
+   else bw.append("1")
+
+   bw.flush()
+   bw.close()
    close()
+
+
 }
