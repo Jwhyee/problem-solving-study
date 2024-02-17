caee9cd043d42b9dc7c6b8ca9d9570fd62df4103
Jwhyee <kawnsdud@naver.com>
2024-02-17 23:56:40 +0900

    [BOJ][GOL4] - 문자열 지옥에 빠진 호석

--- src/main/kotlin/dfs/BOJ_20166.kt
+++ src/main/kotlin/dfs/BOJ_20166.kt
@@ -3,101 +3,70 @@ package dfs
 import java.lang.StringBuilder
 import java.util.StringTokenizer
 import kotlin.math.max
-import kotlin.math.min
 
 private val dy = intArrayOf(-1, -1, -1, 0, 0, 1, 1, 1)
 private val dx = intArrayOf(-1, 0, 1, -1, 1, -1, 0, 1)
+private val sb = StringBuilder()
+private val strMap = mutableMapOf<String, Int>()
+private lateinit var graph: Array<String>
+private var max = 0
 
 /**
  * 문제 이름(난이도) : 문자열 지옥에 빠진 호석 (GOL4)
- * 시간 : 11 ms
- * 메모리 : 11 KB
+ * 시간 : 528 ms
+ * 메모리 : 68756 KB
  * 링크 : https://www.acmicpc.net/problem/20166
  */
-fun main() = with(System.`in`.bufferedReader()) {
+fun main() = with(System.`in`.bufferedReader()){
+   val bw = System.out.bufferedWriter()
    val (n, m, k) = StringTokenizer(readLine()).run {
       Triple(nextToken().toInt(), nextToken().toInt(), nextToken().toInt())
    }
 
-   val map = Array(n) { CharArray(m) }
-
-   repeat(n) { map[it] = readLine().toCharArray() }
-
-   var min = Int.MAX_VALUE
-   var max = Int.MIN_VALUE
-
-   val godsLikesString = mutableMapOf<String, Int>()
-
-   repeat(k) {
-      val str = readLine()
-      godsLikesString[str] = 0
-      min = min(min, str.length)
-      max = max(max, str.length)
-   }
-
-   val range = min..max
-
-   val sb = StringBuilder()
-
-   lateinit var visited: Array<BooleanArray>
-
-   fun backTracking(depth: Int, y: Int, x: Int) {
-      if (depth + 1 in range) {
-         val cur = sb.toString()
-         if(godsLikesString.containsKey(cur)) {
-            godsLikesString[cur] = godsLikesString[cur]!! + 1
-         }
-         return
-      }
-
-      visited[y][x] = true
-
-      for (i in 0 until 8) {
-         val next = nextPos(y, x, i, n, m)
-         val ny = next.first
-         val nx = next.second
-         sb.append(map[ny][nx])
-         backTracking(depth + 1, ny, nx)
-         sb.deleteCharAt(depth + 1)
-      }
-
-   }
-
-   val startsSet = godsLikesString.map { it.key[0] }.toSet()
+   graph = Array(n) { "" }
 
    for (i in 0 until n) {
-      for (j in 0 until m) {
-         val cur = map[i][j]
+      graph[i] = readLine()
+   }
 
-         if (startsSet.contains(cur)) {
-            sb.clear()
-            visited = Array(n) { BooleanArray(m) }
-            sb.append(cur)
-            backTracking(0, i, j)
-         }
+   val godStr = Array(k) { "" }
 
+   for (i in 0 until k) {
+      godStr[i] = readLine()
+      max = max(max, godStr[i].length)
+   }
+
+   for (i in graph.indices) {
+      for (j in graph[i].indices) {
+         sb.clear()
+         sb.append(graph[i][j])
+         dfs(0, i, j, n, m)
       }
    }
 
-   val bw = System.out.bufferedWriter()
-   for (value in godsLikesString.values) {
-      bw.append("$value")
-      bw.newLine()
+   for(i in 0 until k){
+      if (strMap[godStr[i]] == null) {
+         bw.append("0\n")
+      } else bw.append("${strMap[godStr[i]]}\n")
    }
 
    bw.flush()
    bw.close()
    close()
-
 }
-private fun nextPos(y: Int, x: Int, dir: Int, n: Int, m: Int): Pair<Int, Int> {
-   val nx = x + dx[dir]
-   val ny = y + dy[dir]
 
-//   println("cur ($ny, $nx)")
-   val cy = if(ny < 0) n + (ny % n) else if (ny > n - 1) ny % n else ny
-   val cx = if(nx < 0) m + (nx % m) else if (nx > m - 1) nx % m else nx
-//   println("cal ($cy, $cx)")
+private fun dfs(depth: Int, y: Int, x: Int, n: Int, m: Int) {
+   val cur = sb.toString()
+   strMap[cur] = strMap.getOrDefault(cur, 0) + 1
 
-   return (cy to cx)
+   if (cur.length == max) return
+
+   for (i in 0 until 8) {
+      val ny = (y + dy[i] + n) % n
+      val nx = (x + dx[i] + m) % m
+      sb.append(graph[ny][nx])
+      dfs(depth + 1, ny, nx, n, m)
+      sb.deleteCharAt(depth + 1)
+   }
+
 }
