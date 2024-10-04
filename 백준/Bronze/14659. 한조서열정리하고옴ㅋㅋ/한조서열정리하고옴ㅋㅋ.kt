ce9453aa270bbb502d9e76c8c738b8dc3e5120c4
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2024-10-04 22:14:49 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 496 ms, Memory: 26400 KB -BaekjoonHub

---
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.kt
@@ -0,0 +1,19 @@
+import kotlin.math.max
+
+fun main() = with(System.`in`.bufferedReader()){
+    val n = readLine().toInt()
+    var max = 0
+    val arr = readLine().split(" ").map { it.toInt() }.toTypedArray()
+    for (i in arr.indices) {
+        val cur = arr[i]
+        var cnt = 0
+        for (j in (i + 1) until arr.size) {
+            val next = arr[j]
+            if(cur > next) {
+                cnt++
+            } else break
+        }
+        max = max(max, cnt)
+    }
+    println(max)
+}
