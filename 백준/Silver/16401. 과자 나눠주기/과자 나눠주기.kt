2c4e22a65775c0492d9792c71ed7dab5878aed21
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2025-02-02 15:22:34 +0900

    [Silver II] Title: 과자 나눠주기, Time: 716 ms, Memory: 177876 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.kt
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.kt
@@ -1,18 +1,22 @@
 import java.util.StringTokenizer
+import kotlin.math.max
 
 fun main() = with(System.`in`.bufferedReader()) {
     val (n, m) = StringTokenizer(readLine()).run {
         nextToken().toInt() to nextToken().toInt()
     }
-    var sum = 0
+    var max = 0
     val list = StringTokenizer(readLine()).run {
         List(m) {
-            nextToken().toInt()
+            val num = nextToken().toInt()
+            max = max(max, num)
+            num
         }
     }
 
     var left = 1
-    var right = list.maxOrNull()!!
+    var right = max
+    max = 0
     while (left <= right) {
         val mid = (left + right) / 2
         var cnt = 0
@@ -20,11 +24,11 @@ fun main() = with(System.`in`.bufferedReader()) {
             cnt += (i / mid)
         }
         if (cnt >= n) {
-            sum = mid
+            max = mid
             left = mid + 1
         } else {
             right = mid - 1
         }
     }
-    println(sum)
+    println(max)
 }
