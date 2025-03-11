d3402468e13978f6ae50e8b4188e222910456e37
JunYoung Ka <kawnsdud@naver.com>
2025-03-11 10:35:25 +0900

    [GOL4] - 로하의 농사

--- src/main/kotlin/back_tracking/BOJ_26153.kt
+++ src/main/kotlin/back_tracking/BOJ_26153.kt
@@ -6,7 +6,7 @@ private val dx = intArrayOf(1, 0, -1, 0)
 private val dy = intArrayOf(0, 1, 0, -1)
 private var max = 0
 
-fun main() = with(System.`in`.bufferedReader()){
+fun main() = with(System.`in`.bufferedReader()) {
     var st = StringTokenizer(readLine())
     val (n, m) = st.nextToken().toInt() to st.nextToken().toInt()
 
@@ -23,35 +23,27 @@ fun main() = with(System.`in`.bufferedReader()){
     st = StringTokenizer(readLine())
     val (x, y, p) = Triple(st.nextToken().toInt(), st.nextToken().toInt(), st.nextToken().toInt())
 
-    backTracking(x, y, p, map, visited, map[x][y])
+    visited[x][y] = true
+    backTracking(x, y, p, map, visited, map[x][y], -1)
     println(max)
 }
 
-private fun backTracking(x: Int, y: Int, pipe: Int, map: Array<IntArray>, visited: Array<BooleanArray>, sum: Int = 0, dir: Int = 0) {
-    if(pipe == 0) {
-        max = maxOf(sum, max)
-        return
-    }
+private fun backTracking(x: Int, y: Int, pipe: Int, map: Array<IntArray>, visited: Array<BooleanArray>, sum: Int, dir: Int) {
+    max = maxOf(sum, max)
 
-    visited[x][y] = true
+    if (pipe <= 0) return
 
     for (i in 0 until 4) {
         val nx = x + dx[i]
         val ny = y + dy[i]
 
-        if(nx < 0 || nx >= map.size || ny < 0 || ny >= map[0].size || visited[nx][ny]) {
-            max = maxOf(sum, max)
-            continue
-        }
+        if (nx !in map.indices || ny !in map[0].indices || visited[nx][ny]) continue
 
-        val curPipe = (if(ny == y || nx == x) pipe - 1 else pipe - 2) - if(dir != i) 1 else 0
-        if(curPipe < 0) {
-            max = maxOf(sum, max)
-            continue
-        }
+        val cost = if (dir == -1 || dir == i) 1 else 2
+        if (pipe < cost) continue
 
         visited[nx][ny] = true
-        backTracking(nx, ny, curPipe, map, visited, sum + map[nx][ny], i)
+        backTracking(nx, ny, pipe - cost, map, visited, sum + map[nx][ny], i)
         visited[nx][ny] = false
     }
 }
