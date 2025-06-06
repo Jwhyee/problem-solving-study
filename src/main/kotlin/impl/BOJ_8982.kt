b586cf53320575f7c89410a322747207d5f16101
Jwhyee <kawnsdud@naver.com>
2025-06-06 17:02:32 +0900

    [BOJ][GOL3] - 수족관1(100점)

--- src/main/kotlin/impl/BOJ_8982.kt
+++ src/main/kotlin/impl/BOJ_8982.kt
@@ -1,116 +1,94 @@
 package impl
 
-import java.util.LinkedList
-import java.util.Queue
-import java.util.StringTokenizer
-
-private data class Position(
-    val x: Int,
-    val y: Int
-)
+import java.util.*
 
 fun main() = with(System.`in`.bufferedReader()) {
-    val n = readLine().toInt()
+    var st = StringTokenizer(readLine())
+    val N = st.nextToken().toInt()
 
-    val positions = Array(n) { Position(-1, -1) }
-
-    var st: StringTokenizer
-
-    var maxX = Int.MIN_VALUE
-    var maxY = Int.MIN_VALUE
-
-    repeat(n) {
+    data class Point(val x: Int, val y: Int)
+    val positions = Array(N) { Point(-1, -1) }
+    repeat(N) { i ->
         st = StringTokenizer(readLine())
         val x = st.nextToken().toInt()
         val y = st.nextToken().toInt()
-
-        maxX = maxOf(maxX, x)
-        maxY = maxOf(maxY, y)
-
-        positions[it] = Position(x, y)
+        positions[i] = Point(x, y)
     }
 
-    val map = Array(maxY + 1) { IntArray(maxX) { -1 } }
+    val segCount = (N - 1) / 2
 
-    // y 좌표 기준으로 가장 밑에서 높은 좌표 찾기
-    var maxHeight = Int.MIN_VALUE
-    for(i in 0 until positions.size - 1) {
+    val arrX1 = IntArray(segCount)
+    val arrX2 = IntArray(segCount)
+    val segY  = IntArray(segCount)
+
+    val h = BooleanArray(segCount) { false }
+
+    val dSecond = IntArray(segCount) { 0 }
+
+    var idx = 0
+    for (i in 0 until N - 1) {
         val (x1, y1) = positions[i]
         val (x2, y2) = positions[i + 1]
-
-        if (x1 == x2) {
-            maxHeight = maxOf(maxHeight, y1, y2)
-        } else {
-            for (j in x1 until x2) {
-                for (k in 0 until minOf(maxHeight, y1, y2)) {
-                    map[k][j] = 1
-                }
-            }
+        if (y1 == y2) {
+            arrX1[idx] = minOf(x1, x2)
+            arrX2[idx] = maxOf(x1, x2)
+            segY[idx]  = y1
+            idx++
         }
-
     }
 
-    val k = readLine().toInt()
-    val holes = Array(k) { Position(1, 1) }
-    repeat(k) {
+    st = StringTokenizer(readLine())
+    val K = st.nextToken().toInt()
+
+    repeat(K) {
         st = StringTokenizer(readLine())
-        val (x1, y1) = st.nextToken().toInt() to st.nextToken().toInt()
-        val (x2, _) = st.nextToken().toInt() to st.nextToken().toInt()
+        val hx1 = st.nextToken().toInt()
+        val hy  = st.nextToken().toInt()
+        val hx2 = st.nextToken().toInt()
 
-        for (x in x1 until x2) {
-            map[y1][x] = -2
-            holes[it] = Position(x, y1)
-        }
-    }
-
-    bfs(map, holes)
-
-    var sum = 0
-    for (i in map.indices) {
-        for (j in map[i].indices) {
-            if (map[i][j] == 1) {
-                sum++
+        for (i in 0 until segCount) {
+            if (arrX1[i] == hx1 && arrX2[i] == hx2 && segY[i] == hy) {
+                h[i] = true
+                break
             }
         }
     }
-    println(sum)
-}
 
-private val dx = intArrayOf(1, 0, -1)
-private val dy = intArrayOf(0, -1, 0)
-
-private fun bfs(map: Array<IntArray>, holes: Array<Position>) {
-    val visited = Array(map.size) { BooleanArray(map[0].size) }
-
-    for (hole in holes) {
-        val queue: Queue<Position> = LinkedList()
-
-        queue += hole
-
-        while (queue.isNotEmpty()) {
-            val current = queue.poll()
-            val (x, y) = current
-
-            visited[y][x] = true
-
-            for (i in 0 until 3) {
-                val ny = y + dy[i]
-                val nx = x + dx[i]
-
-                if (ny < 0 || ny >= map.size || nx < 0 || nx >= map[0].size || map[ny][nx] == -1 || map[ny][nx] == -2) continue
-
-                if(!visited[ny][nx] && hole.y > ny) {
-                    map[ny][nx] = -1
-                    queue += Position(nx, ny)
-                }
+    var water = 0
+    for (i in segCount - 1 downTo 0) {
+        if (h[i]) {
+            water = segY[i]
+        } else {
+            if (water > segY[i]) {
+                water = segY[i]
             }
         }
+        dSecond[i] = water
     }
-}
 
-private fun printMap(map: Array<IntArray>) {
-    println("============================")
-    for (i in map.indices) {
-        println(map[i].joinToString(" ") { if (it == -1) "." else if (it == -2) "X" else "O" })
+    water = 0
+    for (i in 0 until segCount) {
+        if (h[i]) {
+            water = segY[i]
+        } else {
+            if (water > segY[i]) {
+                water = segY[i]
+            }
+        }
+        if (dSecond[i] > water) {
+            water = dSecond[i]
+        }
+        dSecond[i] = water
     }
+
+    var answer = 0L
+    for (i in 0 until segCount) {
+        val width  = arrX2[i] - arrX1[i]
+        val height = segY[i] - dSecond[i]
+        if (height > 0) {
+            answer += width.toLong() * height.toLong()
+        }
+    }
+
+    println(answer)
 }
