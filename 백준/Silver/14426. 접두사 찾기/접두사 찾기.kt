2d29ce5dc4d39ba34ab99d6320f80e6d5bd12b15
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2025-11-15 12:37:26 +0900

    [Silver I] Title: 접두사 찾기, Time: 360 ms, Memory: 43496 KB -BaekjoonHub

---
+++ 백준/Silver/14426. 접두사 찾기/접두사 찾기.kt
@@ -0,0 +1,22 @@
+import java.util.StringTokenizer
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (n, m) = StringTokenizer(readLine()).run {
+        nextToken().toInt() to nextToken().toInt()
+    }
+
+    val targetList = ArrayList<String>()
+    repeat(n) { targetList += readLine() }
+    targetList.sort()
+
+    var count = 0
+    repeat(m) {
+        val q = readLine()
+        val idx = targetList.binarySearch(q).let { if (it < 0) -it - 1 else it }
+
+        if (idx < targetList.size && targetList[idx].startsWith(q)) {
+            count++
+        }
+    }
+    println(count)
+}
