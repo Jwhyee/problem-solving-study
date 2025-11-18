a0514857940bb2519e59cc0eeb49620a9004da59
Jwhyee <kawnsdud@naver.com>
2025-11-18 18:43:23 +0900

    [BOJ][GOL1] - 아이템 획득

--- src/main/kotlin/binary_search/BOJ_28216.kt
+++ src/main/kotlin/binary_search/BOJ_28216.kt
@@ -1,132 +1,81 @@
 package binary_search
 
+import java.io.BufferedReader
 import java.util.*
 
-private data class Item(val target: Int, var w: Int)
+private const val N_MAX = 200010
 
-private data class Car(var x: Int, var y: Int) {
-    fun move(dir: Int, moveCnt: Int) {
-        x += dx[dir] * moveCnt
-        y += dy[dir] * moveCnt
-    }
-}
+private val list = Array(N_MAX) { Array(2) { mutableListOf<Pair<Int, Long>>() } }
+private val dx = intArrayOf(1, 0, -1, 0)
+private val dy = intArrayOf(0, 1, 0, -1)
 
-private fun lowerBound(list: List<Item>, target: Int): Int {
-    var left = 0
-    var right = list.size
+private fun binarySearch(idx: Int, sw: Int, s: Int): Long {
+    val list = list[idx][sw]
+    var l = 0
+    var r = list.size - 1
+    var mid: Int
 
-    while (left < right) {
-        val mid = (left + right) / 2
-        if (list[mid].target < target) {
-            left = mid + 1
-        } else {
-            right = mid
-        }
+    while (l <= r) {
+        mid = (l + r) / 2
+        if (list[mid].first <= s) l = mid + 1
+        else r = mid - 1
     }
 
-    return left
+    return if (r < 0) 0 else list[r].second
 }
 
-private fun upperBound(list: List<Item>, target: Int): Int {
-    var left = 0
-    var right = list.size
-
-    while (left < right) {
-        val mid = (left + right) / 2
-        if (list[mid].target <= target) {
-            left = mid + 1
-        } else {
-            right = mid
-        }
-    }
-
-    return left
-}
-
-private fun getResult(items: List<Item>, min: Int, max: Int): Int {
-    val startIdx = lowerBound(items, min)
-    val endIdx = upperBound(items, max)
-
-    if (startIdx >= items.size || endIdx == 0) return 0
-    if (endIdx > items.size) return items.last().w - if (startIdx > 0) items[startIdx - 1].w else 0
-
-    return if (startIdx == 0) {
-        items[endIdx - 1].w
-    } else {
-        items[endIdx - 1].w - items[startIdx - 1].w
-    }
-}
-
-private val dx = arrayOf(1, 0, -1, 0)
-private val dy = arrayOf(0, 1, 0, -1)
-
-fun main() = with(System.`in`.bufferedReader()) {
-    val (n, q) = StringTokenizer(readLine()).run {
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    var st = StringTokenizer(readLine())
+    val (n, q) = st.run {
         nextToken().toInt() to nextToken().toInt()
     }
 
-    val xMap = mutableMapOf<Int, MutableList<Item>>()
-    val yMap = mutableMapOf<Int, MutableList<Item>>()
-
+    // 아이템 입력
     repeat(n) {
-        val (x, y, w) = StringTokenizer(readLine()).run {
-            Triple(nextToken().toInt(), nextToken().toInt(), nextToken().toInt())
+        st = StringTokenizer(readLine())
+        val (x, y, w) = st.run {
+            Triple(nextToken().toInt(), nextToken().toInt(), nextToken().toLong())
         }
-        xMap.computeIfAbsent(x) { mutableListOf() }.add(Item(y, w))
-        yMap.computeIfAbsent(y) { mutableListOf() }.add(Item(x, w))
+
+        // val[x][0] : x 기준 y를 저장
+        // val[y][1] : y 기준 x를 저장
+        list[x][0].add(Pair(y, w))
+        list[y][1].add(Pair(x, w))
     }
 
-    // 누적 합 계산 (아이템을 빠르게 합산할 수 있도록)
-    xMap.forEach { (_, value) ->
-        value.sortBy { it.target }
-        for (i in 1 until value.size) {
-            value[i].w += value[i - 1].w
-        }
-    }
-    yMap.forEach { (_, value) ->
-        value.sortBy { it.target }
-        for (i in 1 until value.size) {
-            value[i].w += value[i - 1].w
+    // 정렬 + 누적합
+    for (i in 1 until N_MAX) {
+        for (j in 0..1) {
+            val list = list[i][j]
+            list.sortBy { it.first }
+            for (k in 1 until list.size) {
+                val prev = list[k - 1].second
+                list[k] = Pair(list[k].first, list[k].second + prev)
+            }
         }
     }
 
-    val car = Car(1, 1)
-    var result = 0
+    var posX = 1
+    var posY = 1
+    var ans = 0L
 
     repeat(q) {
-        val (dir, moveCnt) = StringTokenizer(readLine()).run {
-            nextToken().toInt() to nextToken().toInt()
+        st = StringTokenizer(readLine())
+        val d = st.nextToken().toInt()
+        val v = st.nextToken().toInt()
+
+        // 아이템 회수
+        ans += when (d) {
+            0 -> binarySearch(posY, 1, posX + v) - binarySearch(posY, 1, posX)
+            1 -> binarySearch(posX, 0, posY + v) - binarySearch(posX, 0, posY)
+            2 -> binarySearch(posY, 1, posX - 1) - binarySearch(posY, 1, posX - v - 1)
+            else -> binarySearch(posX, 0, posY - 1) - binarySearch(posX, 0, posY - v - 1)
         }
 
-        val startX = car.x
-        val endX = car.x + dx[dir] * moveCnt
-        val startY = car.y
-        val endY = car.y + dy[dir] * moveCnt
-
-        val minX = minOf(startX, endX)
-        val maxX = maxOf(startX, endX)
-        val minY = minOf(startY, endY)
-        val maxY = maxOf(startY, endY)
-
-        val r = when (dir) {
-            0 -> yMap[car.y]?.let { items ->
-                getResult(items, if (minX == car.x) minX + 1 else minX, maxX)
-            } ?: 0
-            1 -> xMap[car.x]?.let { items ->
-                getResult(items, if (minY == car.y) minY + 1 else minY, maxY)
-            } ?: 0
-            2 -> yMap[car.y]?.let { items ->
-                getResult(items, minX, if (maxX == car.x) maxX - 1 else maxX)
-            } ?: 0
-            3 -> xMap[car.x]?.let { items ->
-                getResult(items, minY, if (maxY == car.y) maxY - 1 else maxY)
-            } ?: 0
-            else -> 0
-        }
-        result += r
-
-        car.move(dir, moveCnt)
+        // 위치 이동
+        posX += dx[d] * v
+        posY += dy[d] * v
     }
 
-    println(result)
+    print(ans)
 }
