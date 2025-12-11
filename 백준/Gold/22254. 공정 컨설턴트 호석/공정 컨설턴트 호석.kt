2e9e25d55f45458f6cf4e4fbcc3afb599bff7472
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2025-12-11 22:35:12 +0900

    [Gold III] Title: 공정 컨설턴트 호석, Time: 604 ms, Memory: 77684 KB -BaekjoonHub

---
+++ 백준/Gold/22254. 공정 컨설턴트 호석/공정 컨설턴트 호석.kt
@@ -0,0 +1,56 @@
+import java.util.PriorityQueue
+import java.util.StringTokenizer
+
+fun main() = with(System.`in`.bufferedReader()) {
+    var st = StringTokenizer(readLine())
+    val (n, x) = st.run {
+        nextToken().toInt() to nextToken().toInt()
+    }
+
+    val times = IntArray(n) { 0 }
+
+    st = StringTokenizer(readLine())
+    repeat(n) {
+        times[it] = st.nextToken().toInt()
+    }
+
+    println(binarySearch(n, x, times))
+
+    close()
+}
+
+private fun check(n: Int, x: Int, mid: Int, times: IntArray): Boolean {
+    val pq = PriorityQueue<Int>()
+
+    for (i in 0..mid) {
+        pq += times[i]
+    }
+
+    for (i in mid + 1 until n) {
+        val cur = pq.poll()
+
+        if(cur + times[i] > x) {
+            return false
+        }
+        pq += (cur + times[i])
+    }
+
+    return true
+}
+
+private fun binarySearch(n: Int, x: Int, times: IntArray): Int {
+    var left = 0
+    var right = n
+
+    while (left < right) {
+        val mid = (left + right) / 2
+
+        if (check(n, x, mid, times)) {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+
+    return right + 1
+}
