d06dc1915ab0408910b3cfee65e96fa5feb4cc00
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2026-01-19 17:33:03 +0900

    [Gold III] Title: 순회강연, Time: 244 ms, Memory: 23680 KB -BaekjoonHub

---
+++ 백준/Gold/2109. 순회강연/순회강연.kt
@@ -0,0 +1,31 @@
+import java.util.*
+
+private data class Lecture(
+    val pay: Int,
+    val day: Int
+)
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val line = readLine() ?: return
+    val n = line.toInt()
+    val lectures = mutableListOf<Lecture>()
+
+    repeat(n) {
+        val st = StringTokenizer(readLine())
+        lectures.add(Lecture(st.nextToken().toInt(), st.nextToken().toInt()))
+    }
+
+    lectures.sortBy { it.day }
+
+    val pq = PriorityQueue<Int>()
+
+    for (lecture in lectures) {
+        pq.add(lecture.pay)
+        
+        if (pq.size > lecture.day) {
+            pq.poll()
+        }
+    }
+
+    println(pq.sum())
+}
