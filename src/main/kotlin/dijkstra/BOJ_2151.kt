c1b759402cd12cb7bc6a7a4dfd7ef6c24a6088a6
Jwhyee <kawnsdud@naver.com>
2026-01-22 16:01:05 +0900

    [BOJ][GOL3] - 거울 설치

---
+++ src/main/kotlin/dijkstra/BOJ_2151.kt
@@ -0,0 +1,96 @@
+package dijkstra
+
+import java.util.PriorityQueue
+
+private const val WALL = '*'
+private const val EMPTY = '.'
+private const val DOOR = '#'
+private const val MIRROR = '!'
+
+fun main() = with(System.`in`.bufferedReader()){
+    val n = readLine().toInt()
+
+    val doors = ArrayList<Pair<Int, Int>>(2)
+
+    val map = Array(n) { i ->
+        val line = readLine()
+        CharArray(n) { j ->
+            if (line[j] == DOOR) {
+                doors.add(i to j)
+            }
+            line[j]
+        }
+    }
+
+    println(dijkstra(map, doors, n))
+
+    close()
+}
+
+private val dy = intArrayOf(1, 0, -1, 0)
+private val dx = intArrayOf(0, -1, 0, 1)
+
+private fun nextDir(dir: Int) = when(dir) {
+    0 -> 1 to 3
+    1 -> 2 to 0
+    2 -> 3 to 1
+    3 -> 0 to 2
+    else -> -1 to -1
+}
+
+private fun dijkstra(
+    map: Array<CharArray>,
+    doors: ArrayList<Pair<Int, Int>>,
+    n: Int
+): Int {
+    // y, x, (direction, mirrorCount)
+    val pq = PriorityQueue<Triple<Int, Int, Pair<Int, Int>>>(compareBy { it.third.second })
+    val dists = Array(n) { Array(n) { IntArray(4) { Int.MAX_VALUE } } }
+
+    val (entryY, entryX) = doors[0]
+    val (exitY, exitX) = doors[1]
+
+    for (dir in 0 until 4) {
+        dists[entryY][entryX][dir] = 0
+        pq.add(Triple(entryY, entryX, dir to 0))
+    }
+
+    while (pq.isNotEmpty()) {
+        val (y, x, mirrorInfo) = pq.poll()
+        val (dir, mirrorCount) = mirrorInfo
+
+        if (mirrorCount > dists[y][x][dir]) {
+            continue
+        }
+
+        val ny = y + dy[dir]
+        val nx = x + dx[dir]
+
+        if (ny in 0 until n && nx in 0 until n && map[ny][nx] != WALL) {
+            // 1. 거울을 설치하지 않고 직진하는 경우
+            if (dists[ny][nx][dir] > mirrorCount) {
+                dists[ny][nx][dir] = mirrorCount
+                pq.add(Triple(ny, nx, dir to mirrorCount))
+            }
+
+            // 2. 거울을 설치하고 방향을 바꾸는 경우
+            if (map[ny][nx] == MIRROR) {
+                val (opt1, opt2) = nextDir(dir)
+                val nextMirrorCount = mirrorCount + 1
+
+                // 방향 전환 1
+                if (dists[ny][nx][opt1] > nextMirrorCount) {
+                    dists[ny][nx][opt1] = nextMirrorCount
+                    pq.add(Triple(ny, nx, opt1 to nextMirrorCount))
+                }
+                // 방향 전환 2
+                if (dists[ny][nx][opt2] > nextMirrorCount) {
+                    dists[ny][nx][opt2] = nextMirrorCount
+                    pq.add(Triple(ny, nx, opt2 to nextMirrorCount))
+                }
+            }
+        }
+    }
+
+    return dists[exitY][exitX].minOrNull() ?: -1
+}
