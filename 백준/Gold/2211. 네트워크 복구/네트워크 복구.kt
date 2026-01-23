28bede872ec32376a96c9b70caa3777e2768ffbd
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2026-01-23 18:29:46 +0900

    [Gold II] Title: 네트워크 복구, Time: 628 ms, Memory: 96008 KB -BaekjoonHub

---
+++ 백준/Gold/2211. 네트워크 복구/네트워크 복구.kt
@@ -0,0 +1,70 @@
+import java.util.PriorityQueue
+import java.util.StringTokenizer
+
+private data class Network(
+    val to: Int, val cost: Int
+)
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (n, m) = StringTokenizer(readLine()).run {
+        nextToken().toInt() to nextToken().toInt()
+    }
+
+    val networks = Array(n + 1) { arrayListOf<Network>() }
+
+    repeat(m) {
+        val st = StringTokenizer(readLine())
+
+        val computer1 = st.nextToken().toInt()
+        val computer2 = st.nextToken().toInt()
+        val weight = st.nextToken().toInt()
+
+        networks[computer1].add(Network(computer2, weight))
+        networks[computer2].add(Network(computer1, weight))
+    }
+
+    val prev = IntArray(n + 1) { 0 }
+
+    dijkstra(networks, prev, n)
+
+    println(n - 1)
+    for (node in 2..n) {
+        println("$node ${prev[node]}")
+    }
+
+    close()
+}
+
+private fun dijkstra(
+    networks: Array<ArrayList<Network>>,
+    prev: IntArray,
+    n: Int
+): Int {
+    // to, cost
+    val pq = PriorityQueue<Pair<Int, Int>>(compareBy { it.second })
+    val dist = Array(n + 1) { Int.MAX_VALUE }
+    dist[1] = 0
+
+    pq.add(1 to 0)
+
+    while (pq.isNotEmpty()) {
+        val (curNode, curCost) = pq.poll()
+
+        if (dist[curNode] < curCost) continue
+
+        val networkList = networks[curNode]
+
+        for ((nextNode, cost) in networkList) {
+            val nextCost = curCost + cost
+
+            if (dist[nextNode] > nextCost) {
+                prev[nextNode] = curNode
+
+                dist[nextNode] = nextCost
+                pq.add(nextNode to nextCost)
+            }
+        }
+    }
+
+    return -1
+}
