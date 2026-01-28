a4c9f9c3f97bf1b17085748d1eed4bec1caf23eb
Jwhyee <kawnsdud@naver.com>
2026-01-28 16:31:11 +0900

    [BOJ][GOL5] - 상어 초등학교

--- src/main/kotlin/impl/BOJ_21608.kt
+++ src/main/kotlin/impl/BOJ_21608.kt
@@ -1,101 +1,129 @@
 package impl
 
 import java.util.StringTokenizer
+import kotlin.math.pow
 
-/**
- * 문제 이름(난이도) : 상어 초등학교(GOL5)
- * 시간 : 1 ms
- * 메모리 : 1 KB
- * 링크 : https://www.acmicpc.net/problem/21608
- */
-private var n = 0
-private val students = ArrayList<Pair<Int, MutableList<Int>>>()
-private lateinit var classRoom: Array<IntArray>
+private val dy = intArrayOf(0, 1, 0, -1)
+private val dx = intArrayOf(1, 0, -1, 0)
+
+private data class Seat(
+    val favoriteCount: Int,
+    val emptyCount: Int,
+    val row: Int,
+    val col: Int
+)
+
 fun main() = with(System.`in`.bufferedReader()) {
-    n = readLine()!!.toInt()
-    classRoom = Array(n) { IntArray(n) }
-    val total = n * n
+    val n = readLine().toInt()
+    val preferenceGraph = IntArray(n * n) { 0 }
+    val sortedGraph = Array(n * n) { BooleanArray(n * n + 1) { false } }
 
-    // 그래프 생성
-    repeat(total) { i ->
+    repeat(n * n) {
         val st = StringTokenizer(readLine())
-        val cur = st.nextToken().toInt()
-        students += (cur to mutableListOf())
-        repeat(4) { j ->
-            students[i].second.add(st.nextToken().toInt())
-        }
+
+        val target = st.nextToken().toInt()
+        val index = target - 1
+        val s1 = st.nextToken().toInt()
+        val s2 = st.nextToken().toInt()
+        val s3 = st.nextToken().toInt()
+        val s4 = st.nextToken().toInt()
+
+        preferenceGraph[it] = target
+
+        sortedGraph[index][s1] = true
+        sortedGraph[index][s2] = true
+        sortedGraph[index][s3] = true
+        sortedGraph[index][s4] = true
     }
 
-    for (student in students) {
-        // 1. 비어있는 칸 중에서 좋아하는 학생이 인접한 칸에 가장 많은 칸으로 자리를 정한다.
-        var flagWithPos = findSeat(student.second)
-        var flag = flagWithPos.first
-        var pos = flagWithPos.second
+    val classroom = Array(n) { IntArray(n) { 0 } }
 
-        if (!flag) {
-            // 2. 1을 만족하는 칸이 여러 개이면, 인접한 칸 중에서 비어있는 칸이 가장 많은 칸으로 자리를 정한다.
-            flagWithPos = findSeat(student.second, true)
-            flag = flagWithPos.first
-            pos = flagWithPos.second
+    for (student in preferenceGraph) {
+        val preferences = sortedGraph[student - 1]
+        targetPosition(n, student, preferences, classroom)
+    }
 
-            if(!flag) {
-                // 3. 2를 만족하는 칸도 여러 개인 경우에는 행의 번호가 가장 작은 칸으로, 그러한 칸도 여러 개이면 열의 번호가 가장 작은 칸으로 자리를 정한다.
+    val sum = getSatisfy(n, sortedGraph, classroom)
 
-            } else {
-                classRoom[pos[0]][pos[1]] = student.first
+    println(sum)
+}
+
+private fun getSatisfy(
+    n: Int,
+    sortedGraph: Array<BooleanArray>,
+    classroom: Array<IntArray>
+): Int {
+    var sum = 0
+
+    for (row in 0 until n) {
+        for (col in 0 until n) {
+            val currentStudent = classroom[row][col]
+            var point = 0
+
+            for (dir in 0..3) {
+                val ny = row + dy[dir]
+                val nx = col + dx[dir]
+
+                if (ny in 0 until n && nx in 0 until n) {
+                    if (sortedGraph[currentStudent - 1][classroom[ny][nx]]) {
+                        point++
+                    }
+                }
             }
-        } else {
-            classRoom[pos[0]][pos[1]] = student.first
-        }
-    }
 
-    for (i in 0 until n) {
-        for (j in 0 until n) {
-            print("${classRoom[i][j]} ")
-        }
-        println()
-    }
-}
-
-fun findSeat(favoriteList: MutableList<Int>, isFindEmptySeat: Boolean = false): Pair<Boolean, IntArray> {
-    var (y, x) = (0 to 0)
-    var max = -1
-    var maxCnt = 0
-
-    for (i in 0 until n) {
-        for (j in 0 until n) {
-            if (classRoom[i][j] != 0) continue
-            val cnt = findAroundStudent(j, i, favoriteList, isFindEmptySeat)
-            if (cnt > max) {
-                max = cnt
-                maxCnt = 1
-                y = i
-                x = j
-            } else if (cnt == max) maxCnt++
-        }
-    }
-
-    if (maxCnt == 1) return (true to intArrayOf(y, x))
-
-    return (false to intArrayOf(0, 0))
-}
-
-fun findAroundStudent(x: Int, y: Int, favoriteList: MutableList<Int>, isFindEmptySeat: Boolean = false): Int {
-    val dx = intArrayOf(1, 0, -1, 0)
-    val dy = intArrayOf(0, 1, 0, -1)
-    var cnt = 0
-
-    for (i in 0..3) {
-        val nx = x + dx[i]
-        val ny = y + dy[i]
-
-        if (nx in 0 until n && ny in 0 until n) {
-            if(isFindEmptySeat) {
-                if (classRoom[ny][nx] == 0) cnt++
-            } else {
-                if (classRoom[ny][nx] != 0 && classRoom[ny][nx] in favoriteList) cnt++
+            if (point > 0) {
+                // 10의 (count-1)승 계산. 1->1, 2->10, 3->100, 4->1000
+                sum += 10.0.pow((point - 1).toDouble()).toInt()
             }
         }
     }
-    return cnt
+
+    return sum
+}
+
+private fun targetPosition(
+    n: Int,
+    student: Int,
+    preferences: BooleanArray,
+    classroom: Array<IntArray>
+) {
+    var bestR = -1
+    var bestC = -1
+    var maxEmptyCount = -1
+    var maxFavoriteCount = -1
+
+    for (row in 0 until n) {
+        for (col in 0 until n) {
+            if (classroom[row][col] != 0) {
+                continue
+            }
+
+            var favoriteCount = 0
+            var emptyCount = 0
+
+            for (dir in 0..3) {
+                val ny = row + dy[dir]
+                val nx = col + dx[dir]
+
+                if (ny in 0 until n && nx in 0 until n) {
+                    if (classroom[ny][nx] == 0) emptyCount++
+                    else if (preferences[classroom[ny][nx]]) {
+                        favoriteCount++
+                    }
+                }
+            }
+
+            if (bestR == -1 ||
+                favoriteCount > maxFavoriteCount ||
+                (favoriteCount == maxFavoriteCount && emptyCount > maxEmptyCount)
+            ) {
+                bestR = row
+                bestC = col
+                maxFavoriteCount = favoriteCount
+                maxEmptyCount = emptyCount
+            }
+        }
+    }
+
+    classroom[bestR][bestC] = student
 }
