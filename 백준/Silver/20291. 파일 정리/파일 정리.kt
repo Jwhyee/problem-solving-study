eda6ff8b504fd06040030f511d65d4c2016ffa0f
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2026-01-28 16:43:47 +0900

    [Silver III] Title: 파일 정리, Time: 596 ms, Memory: 123624 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.kt
+++ 백준/Silver/20291. 파일 정리/파일 정리.kt
@@ -1,25 +1,21 @@
-import java.util.StringTokenizer
+import java.util.*
 
 fun main() = with(System.`in`.bufferedReader()) {
     val sb = StringBuilder()
 
-    val map = mutableMapOf<String, Int>()
+    val map = TreeMap<String, Int>()
     val n = readLine().toInt()
 
     repeat(n) {
-        val (fileName, ext) = StringTokenizer(readLine(), ".").run {
-            nextToken() to nextToken()
-        }
+        val ext = readLine().substringAfterLast(".")
 
-        val cur = (map[ext] ?: 0) + 1
-        map[ext] = cur
+        map[ext] = map.getOrDefault(ext, 0) + 1
     }
 
-    map.keys.sorted().forEach { ext ->
+    for ((ext, count) in map) {
         sb.append("$ext ${map[ext]}").append("\n")
     }
 
     print(sb)
-
     close()
 }
