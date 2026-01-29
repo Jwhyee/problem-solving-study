4e70ef2be244706e17586eba9061d121616c0fc8
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2026-01-29 19:38:09 +0900

    [Gold V] Title: ZOAC, Time: 80 ms, Memory: 12376 KB -BaekjoonHub

---
+++ 백준/Gold/16719. ZOAC/ZOAC.kt
@@ -0,0 +1,29 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    val input = readLine()
+    val visited = BooleanArray(input.length)
+    val sb = StringBuilder()
+
+    fun solve(start: Int, end: Int) {
+        if (start > end) return
+
+        var minCharIdx = start
+        for (i in start..end) {
+            if (input[i] < input[minCharIdx]) {
+                minCharIdx = i
+            }
+        }
+
+        visited[minCharIdx] = true
+
+        for (i in input.indices) {
+            if (visited[i]) sb.append(input[i])
+        }
+        sb.append("\n")
+
+        solve(minCharIdx + 1, end)
+        solve(start, minCharIdx - 1)
+    }
+
+    solve(0, input.length - 1)
+    print(sb)
+}
