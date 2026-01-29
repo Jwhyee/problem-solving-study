f3be6ce8cf9c256a4cc064758d02b01454ed1dc4
Jwhyee <kawnsdud@naver.com>
2026-01-29 15:16:47 +0900

    [BOJ][GOL3] - 경사로

--- src/main/kotlin/impl/BOJ_14890.kt
+++ src/main/kotlin/impl/BOJ_14890.kt
@@ -1,147 +1,55 @@
 package impl
 
-import java.util.*
-
-private val dy = intArrayOf(1, 0)
-private val dx = intArrayOf(0, 1)
+import java.util.StringTokenizer
+import kotlin.math.abs
 
 fun main() = with(System.`in`.bufferedReader()) {
     val (n, l) = StringTokenizer(readLine()).run {
         nextToken().toInt() to nextToken().toInt()
     }
 
-    val check = Array(n) { IntArray(n) { -1 } }
-    val roads = Array(n) {
+    val map = Array(n) {
         val st = StringTokenizer(readLine())
         IntArray(n) { st.nextToken().toInt() }
     }
 
     var count = 0
 
-    // 0번 행의 각 열에서 밑으로 진행
-    for (col in 0 until n) {
-        val result = check(check, roads, 0, col, 0, n, l)
-        if (result) {
-            count++
-        } else {
-            for (row in 0 until n) {
-                check[row][col] = -1
-            }
-        }
-    }
-    // 0번 열의 각 행에서 우측으로 진행
     for (row in 0 until n) {
-        val result = check(check, roads, row, 0, 1, n, l)
-        if (result) {
-            count++
-        } else {
-            for (col in 0 until n) {
-                if(check[row][col] != -1) {
-                    continue
-                } else {
-                    check[row][col] = -1
-                }
-            }
-        }
+        if (canPass(map[row], n, l)) count++
+    }
+
+    for (col in 0 until n) {
+        val columnArray = IntArray(n) { row -> map[row][col] }
+        if (canPass(columnArray, n, l)) count++
     }
 
     println(count)
-
-    close()
 }
 
-private class Road(
-    val row: Int, val col: Int, val useRamp: Boolean, val rampSize: Int,
-)
+private fun canPass(road: IntArray, n: Int, l: Int): Boolean {
+    val visited = BooleanArray(n)
 
-private fun check(
-    check: Array<IntArray>,
-    roads: Array<IntArray>,
-    row: Int, col: Int, dir: Int,
-    n: Int, l: Int
-): Boolean {
-    val queue: Queue<Road> = LinkedList()
+    for (i in 0 until n - 1) {
+        val current = road[i]
+        val next = road[i + 1]
+        val diff = current - next
 
-    queue.add(Road(row, col, false, 0))
+        if (diff == 0) continue
 
-    while(queue.isNotEmpty()) {
-        val road = queue.poll()
+        if (abs(diff) > 1) return false
 
-        val row = road.row
-        val col = road.col
-        val useRamp = road.useRamp
-        val rampSize = road.rampSize
-
-        val height = roads[row][col]
-
-        if (useRamp) {
-            if (check[row][col] == dir) return false
-            check[row][col] = dir
-        }
-
-        val (ny, nx) = row + dy[dir] to col + dx[dir]
-
-        if (ny in 0 until n && nx in 0 until n) {
-            val nextHeight = roads[ny][nx]
-
-            val diff = height - nextHeight
-
-            when(diff) {
-                -1 -> {
-                    val (targetY, targetX) = when(dir) {
-                        0 -> { ny - l to nx }
-                        1 -> { ny to nx - l }
-                        else -> return false
-                    }
-                    if (targetY in 0 until n && targetX in 0 until n) {
-                        when(dir) {
-                            0 -> {
-                                for (y in targetY until ny) {
-                                    if(check[y][targetX] == 0) return false
-                                    else check[y][targetX] = 0
-                                }
-                            }
-                            1 -> {
-                                for (x in targetX until nx) {
-                                    if(check[targetY][x] == 1) return false
-                                    else check[targetY][x] = 1
-                                }
-                            }
-                            else -> return false
-                        }
-                        queue.add(Road(ny, nx, false, 0))
-                    } else {
-                        return false
-                    }
-                }
-                0 -> {
-                    if(useRamp) {
-                        when {
-                            rampSize == l -> queue.add(Road(ny, nx, false, 0))
-                            rampSize < l -> {
-                                queue.add(Road(ny, nx, true, rampSize + 1))
-                            }
-                        }
-                    } else {
-                        queue.add(Road(ny, nx, false, 0))
-                    }
-                }
-                1 -> {
-                    if(useRamp) {
-                        when {
-                            rampSize == l -> queue.add(Road(ny, nx, true, 1))
-                            rampSize < l -> return false
-                        }
-                    } else {
-                        queue.add(Road(ny, nx, true, rampSize + 1))
-                    }
-                }
-                else -> return false
+        if (diff == 1) {
+            for (j in 1..l) {
+                if (i + j >= n || road[i + j] != next || visited[i + j]) return false
+                visited[i + j] = true
             }
         } else {
-            if(useRamp && rampSize < l) return false
+            for (j in 0 until l) {
+                if (i - j < 0 || road[i - j] != current || visited[i - j]) return false
+                visited[i - j] = true
+            }
         }
     }
-
     return true
 }
