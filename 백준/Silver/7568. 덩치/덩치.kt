c0126040d3945ef804e2ad4b8c548db508873b4d
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2026-02-06 15:17:43 +0900

    [Silver V] Title: 덩치, Time: 100 ms, Memory: 18660 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.kt
+++ 백준/Silver/7568. 덩치/덩치.kt
@@ -4,9 +4,7 @@ private data class Person(
     val id: Int,
     val weight: Int,
     val height: Int
-) : Comparable<Person> {
-    override fun compareTo(other: Person) = (other.weight - weight) + (other.height - height)
-}
+)
 
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
@@ -21,13 +19,11 @@ fun main() = with(System.`in`.bufferedReader()) {
         people[index] = Person(index + 1, weight, height)
     }
 
-    people.sort()
-
     val ranking = IntArray(n) { Int.MAX_VALUE }
 
     for (i in 0 until n) {
         var rank = 1
-        
+
         val base = people[i]
         val baseIndex = base.id - 1
 
