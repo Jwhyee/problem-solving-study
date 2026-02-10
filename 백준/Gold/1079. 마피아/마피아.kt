5342da16e89cb0f0a20e58e91f2c351c180faba8
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2026-02-10 23:22:08 +0900

    [Gold II] Title: 마피아, Time: 208 ms, Memory: 16680 KB -BaekjoonHub

--- 백준/Gold/1079. 마피아/마피아.kt
+++ 백준/Gold/1079. 마피아/마피아.kt
@@ -1,103 +1,74 @@
 import java.util.StringTokenizer
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.math.max
 
-fun main() = with(System.`in`.bufferedReader()) {
-    // 마피아, 시민
-    // 참가자 짝수 : 밤 -> 아무나 1명 죽임
-    // 참가자 홀수 : 낮 -> 검거할 사람 죽임
-    // 은진이는 마지막으로 남은 마피아
-    // 유죄 지수 : 낮에 시민들이 누굴 죽일지 고를 때 사용 -> 반응은 2차원 배열 R
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val n = readLine().toInt()
 
     val st = StringTokenizer(readLine())
     val status = IntArray(n) { st.nextToken().toInt() }
 
     val guilty = Array(n) {
-        val st = StringTokenizer(readLine())
-        IntArray(n) {
-            st.nextToken().toInt()
-        }
+        val line = StringTokenizer(readLine())
+        IntArray(n) { line.nextToken().toInt() }
     }
 
     val mafia = readLine().toInt()
+    var maxNights = 0
+    val dead = BooleanArray(n)
 
-    var max = 0
-
-    val dead = BooleanArray(n) { false }
-
-    fun game(n: Int, mafia: Int, remainPeopleCount: Int, day: Int) {
-        // 시민이 이긴 경우, 마피아가 이긴 경우
+    fun game(remainCnt: Int, currentNight: Int) {
         if (dead[mafia]) {
-            max = maxOf(max, day)
+            maxNights = max(maxNights, currentNight)
             return
         }
 
-        if (remainPeopleCount % 2 == 0) {
-            // 밤(사람 하나를 죽인 것으로 처리)
+        if (currentNight + (remainCnt / 2) <= maxNights) {
+            return
+        }
+
+        maxNights = max(maxNights, currentNight)
+
+        // 밤(마피아가 사람을 죽이고 유죄 수치 조정)
+        if (remainCnt % 2 == 0) {
             for (target in 0 until n) {
                 if (target != mafia && !dead[target]) {
                     dead[target] = true
-                    addGuiltyScore(guilty, status, dead, n, target)
 
-                    game(n, mafia, remainPeopleCount - 1, day + 1)
+                    for (i in 0 until n) {
+                        if (!dead[i]) status[i] += guilty[target][i]
+                    }
 
-                    restoreGuiltyScore(guilty, status, dead, n, target)
+                    game(remainCnt - 1, currentNight + 1)
+
+                    for (i in 0 until n) {
+                        if (!dead[i]) status[i] -= guilty[target][i]
+                    }
                     dead[target] = false
                 }
             }
-        } else {
-            // 낮(유죄 지수가 가장 높은 사람 사망)
-            var maxStat = 0
-            var maxStatIndex = 0
+        }
+        // 낮 (유죄 수치가 가장 높고, 그 중에 번호가 가장 낮은 사람 죽음)
+        else {
+            var target = -1
+            var maxVal = -1
 
-            for (index in 0 until n) {
-                if (dead[index]) continue
-                if (maxStat < status[index]) {
-                    maxStat = status[index]
-                    maxStatIndex = index
+            for (i in 0 until n) {
+                if (!dead[i] && status[i] > maxVal) {
+                    maxVal = status[i]
+                    target = i
                 }
             }
 
-            if (maxStat != 0) {
-                dead[maxStatIndex] = true
-
-                game(n, mafia, remainPeopleCount - 1, day)
-
-                dead[maxStatIndex] = false
+            if (target != -1) {
+                dead[target] = true
+                game(remainCnt - 1, currentNight)
+                dead[target] = false
             }
         }
     }
 
-    game(n, mafia, n, 0)
-
-    println(max)
-
-    close()
-}
-
-private fun addGuiltyScore(
-    guilty: Array<IntArray>,
-    status: IntArray,
-    dead: BooleanArray,
-    n: Int,
-    die: Int
-) {
-    for (target in 0 until n) {
-        if (!dead[target]) {
-            status[target] += guilty[die][target]
-        }
-    }
-}
-
-private fun restoreGuiltyScore(
-    guilty: Array<IntArray>,
-    status: IntArray,
-    dead: BooleanArray,
-    n: Int,
-    die: Int
-) {
-    for (target in 0 until n) {
-        if (!dead[target]) {
-            status[target] -= guilty[die][target]
-        }
-    }
+    game(n, 0)
+    println(maxNights)
 }
