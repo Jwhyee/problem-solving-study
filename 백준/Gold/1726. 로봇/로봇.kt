d6d6a9a90bef637dc40c8f0712a0931d125be136
JunYoung Ka <82663161+Jwhyee@users.noreply.github.com>
2026-02-14 17:05:41 +0900

    [Gold III] Title: 로봇, Time: 108 ms, Memory: 14776 KB -BaekjoonHub

--- 백준/Gold/1726. 로봇/로봇.kt
+++ 백준/Gold/1726. 로봇/로봇.kt
@@ -1,104 +1,75 @@
 import java.util.*
 
-private val dy = intArrayOf(0, 0, 1, -1)
-private val dx = intArrayOf(1, -1, 0, 0)
-
-private data class Robot2(
-    val y: Int,
-    val x: Int,
-    val dir: Int,
-    val time: Int,
-) {
-    fun dirs() = arrayOf(0 to dir, 1 to turnRight(), 1 to turnLeft(), 2 to flip())
-    private fun turnRight() = when(dir) {
-        0 -> 2
-        1 -> 3
-        2 -> 1
-        3 -> 0
-        else -> -1
-    }
-    private fun turnLeft() = when(dir) {
-        0 -> 3
-        3 -> 1
-        1 -> 2
-        2 -> 0
-        else -> -1
-    }
-    private fun flip() = when(dir) {
-        0 -> 1
-        1 -> 0
-        2 -> 3
-        3 -> 2
-        else -> -1
-    }
+private class Robot2(val y: Int, val x: Int, val dir: Int, val cost: Int) : Comparable<Robot2> {
+    override fun compareTo(other: Robot2): Int = cost - other.cost
 }
 
 fun main() = with(System.`in`.bufferedReader()) {
-    val (m, n) = StringTokenizer(readLine()).run {
-        nextToken().toInt() to nextToken().toInt()
-    }
+    var st = StringTokenizer(readLine())
+    val m = st.nextToken().toInt()
+    val n = st.nextToken().toInt()
 
     val map = Array(m) {
-        val st = StringTokenizer(readLine())
+        st = StringTokenizer(readLine())
         IntArray(n) { st.nextToken().toInt() }
     }
 
     val (ry, rx, rd) = position(readLine())
     val (ey, ex, ed) = position(readLine())
 
-    var min = Int.MAX_VALUE
+    val dy = intArrayOf(0, 0, 1, -1)
+    val dx = intArrayOf(1, -1, 0, 0)
+
     val dist = Array(m) { Array(n) { IntArray(4) { Int.MAX_VALUE } } }
+    val pq = PriorityQueue<Robot2>()
 
-    val robot = Robot2(ry, rx, rd, 0)
+    dist[ry][rx][rd] = 0
+    pq.offer(Robot2(ry, rx, rd, 0))
 
-    robot.dirs().forEach { (cost, dir) ->
-        dist[ry][rx][dir] = cost
-    }
+    while (pq.isNotEmpty()) {
+        val curr = pq.poll()
+        val y = curr.y
+        val x = curr.x
+        val dir = curr.dir
+        val cost = curr.cost
 
-    val queue: Queue<Robot2> = LinkedList()
-    queue.add(robot)
-
-    while (queue.isNotEmpty()) {
-        val robot = queue.poll()
-        val (y, x, dir, time) = robot
-
-        if (dist[y][x][dir] < time) continue
         if (y == ey && x == ex && dir == ed) {
-            min = minOf(min, time)
-            break
+            println(cost)
+            return@with
         }
 
-        for ((cost, nd) in robot.dirs()) {
-            if (y == ey && x == ex) {
-                if (nd == ed) {
-                    min = minOf(min, time + cost)
-                    break
-                } else {
-                    continue
-                }
-            }
-            for (k in 1..3) {
-                val ny = y + (dy[nd] * k)
-                val nx = x + (dx[nd] * k)
+        if (dist[y][x][dir] < cost) continue
 
-                if (ny in 0 until m && nx in 0 until n && map[ny][nx] != 1) {
-                    val nextTime = time + cost + 1
-                    if (dist[ny][nx][nd] > nextTime) {
-                        dist[ny][nx][nd] = nextTime
-                        queue.add(Robot2(ny, nx, nd, nextTime))
-                    }
-                } else {
-                    break
-                }
+        for (k in 1..3) {
+            val ny = y + dy[dir] * k
+            val nx = x + dx[dir] * k
+
+            if (ny !in 0 until m || nx !in 0 until n || map[ny][nx] == 1) break
+
+            val nextCost = cost + 1
+            if (dist[ny][nx][dir] > nextCost) {
+                dist[ny][nx][dir] = nextCost
+                pq.offer(Robot2(ny, nx, dir, nextCost))
+            }
+        }
+
+        for (nd in 0..3) {
+            if (dir == nd) continue
+
+            val turnCost = if (
+                (dir == 0 && nd == 1) || (dir == 1 && nd == 0) ||
+                (dir == 2 && nd == 3) || (dir == 3 && nd == 2)
+            ) 2 else 1
+
+            val nextCost = cost + turnCost
+            if (dist[y][x][nd] > nextCost) {
+                dist[y][x][nd] = nextCost
+                pq.offer(Robot2(y, x, nd, nextCost))
             }
         }
     }
-
-    println(min)
-
-    close()
 }
 
-private fun position(line: String) = StringTokenizer(line).run {
+private fun position(line: String) = StringTokenizer(line).run { 
     Triple(nextToken().toInt() - 1, nextToken().toInt() - 1, nextToken().toInt() - 1)
 }
